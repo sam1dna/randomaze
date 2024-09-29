514cd3ad3d16aecdcda805579d0328af93209fa6
A-Walrus <ofek.r.guy@gmail.com>
2024-09-29 12:43:23 +0300

    start wall drawing

--- code.asm
+++ code.asm
@@ -4,17 +4,34 @@ P486
 STACK 100h
 
 DATASEG
-s1 DB 'Hello, World!',13,10, '$'
 
 CODESEG
+;bx=y mishbatozt
+;cx=x mishbatozt
+proc walldrawer
+	mov ax,320 
+	mul bx
+	add ax,cx
+	shl ax, 2
+	mov bx,ax
+	mov [byte es:bx], 15
 
+    ret
+endp
 start:
 	mov ax, @data
 	mov ds, ax
 
-	lea dx, [s1]
-	mov ah, 09h
-	int 21h
+	mov ax, 13h
+	int 10h
+
+	mov ax, 0a000h
+	mov es, ax
+
+	mov bx, 49
+	mov cx, 79
+	call walldrawer
+    
 exit:
 	mov ax, 4c00h
 	int 21h
